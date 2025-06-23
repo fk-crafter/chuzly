a49e96d6f960b9ae81a61443b85f6f265308e6f8
fk-crafter <fowad.kh@outlook.fr>
2025-06-23 22:09:37 +0200

    fix main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['https://chuzly.app', 'http://localhost:3000'],
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use('/stripe/webhook', bodyParser.raw({ type: 'application/json' }));
 
