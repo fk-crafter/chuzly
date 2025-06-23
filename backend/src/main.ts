7b7e4c0730e915b75bdfffe6310857482e0926a9
fk-crafter <fowad.kh@outlook.fr>
2025-06-23 22:20:22 +0200

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://chuzly.app'],
+    credentials: true,
+  });
 
   app.use('/stripe/webhook', bodyParser.raw({ type: 'application/json' }));
 
