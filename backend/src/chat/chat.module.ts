90242f4b48b77c85a0edfda5807ed5d51cd2e055
fk-crafter <fowad.kh@outlook.fr>
2025-06-26 22:37:41 +0200

    remove socket io chat

--- backend/src/chat/chat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  providers: [ChatGateway, PrismaService],
-})
-export class ChatModule {}
