8be5bbbfa1da1d1699a46ff0cead4de7de730c32
fk-crafter <fowad.kh@outlook.fr>
2025-06-30 12:40:41 +0200

    feat(CardTransition): adjust before & with cards on mobile

--- frontend/components/Box.tsx
+++ frontend/components/Box.tsx
@@ -1,6 +1,6 @@
 export function Box() {
   return (
-    <div className=" w-[90%] sm:w-[500px] h-[340px] rounded-3xl bg-white dark:bg-zinc-900 border-4 border-primary shadow-xl flex items-center justify-center text-3xl font-bold text-primary">
+    <div className="w-[95%] sm:w-[500px] max-w-sm md:max-w-none h-[300px] md:h-[340px] rounded-3xl bg-white dark:bg-zinc-900 border-4 border-primary shadow-xl flex items-center justify-center text-3xl font-bold text-primary mx-auto">
       Chuzly
     </div>
   );
