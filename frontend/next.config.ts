f1db5058bf296aded21a95da64aedfe399e313b6
fk-crafter <fowad.kh@outlook.fr>
2025-07-17 20:41:37 +0200

    implement pwa

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
-import runtimeCaching from "next-pwa/cache"; // ✅ import des stratégies par défaut
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  /* config options here */
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  runtimeCaching, // ✅ ajoute cette ligne
-})(nextConfig);
+export default nextConfig;
