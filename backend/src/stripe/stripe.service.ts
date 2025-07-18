c3d23b93002562b2374704636d20de1e27f7af9f
fk-crafter <fowad.kh@outlook.fr>
2025-07-18 22:16:21 +0200

    fix(stripe): add new apiversion

--- backend/src/stripe/stripe.service.ts
+++ backend/src/stripe/stripe.service.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 @Injectable()
 export class StripeService {
   private stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-05-28.basil',
+    apiVersion: '2025-06-30.basil',
   });
 
   async createCheckoutSession(userId: string, userEmail: string) {
