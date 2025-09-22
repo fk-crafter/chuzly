0d78d61bb8da6d2b5bbfafc5a27862a879c8e739
fk-crafter <fowad.kh@outlook.fr>
2025-09-22 12:00:53 +0200

    fix: convert mobile from submodule to normal folder

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Tabs } from "expo-router";
+
+const TabGroup = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="(home)"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Tabs.Screen name="products" options={{ title: "Products" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+};
+
+export default TabGroup;
