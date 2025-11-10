b46725446cb5484f628f1315012d51d412d15fc2
fk-crafter <fowad.kh@outlook.fr>
2025-11-10 22:46:11 +0100

    feat(hamburgermenu mobile): use icons instead of text in nav

--- first-rn-app/components/HamburgerMenu.tsx
+++ first-rn-app/components/HamburgerMenu.tsx
@@ -1,14 +1,15 @@
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { Link, usePathname } from "expo-router";
+import { Home, PlusCircle, Settings } from "lucide-react-native";
 
 export default function HamburgerMenu() {
   const pathname = usePathname();
   const cleanPath = pathname.replace("/(protected)", "");
 
   const tabs = [
-    { name: "Overview", path: "/overview" },
-    { name: "Create", path: "/create-event" },
-    { name: "Settings", path: "/setting" },
+    { name: "Overview", path: "/overview", icon: Home },
+    { name: "Create", path: "/create-event", icon: PlusCircle },
+    { name: "Settings", path: "/setting", icon: Settings },
   ];
 
   return (
@@ -17,21 +18,21 @@ export default function HamburgerMenu() {
         const isActive =
           cleanPath === tab.path || cleanPath.startsWith(tab.path + "/");
 
+        const Icon = tab.icon;
+
         return (
           <Link key={tab.name} href={`/(protected)${tab.path}` as any} asChild>
             <TouchableOpacity
               activeOpacity={0.8}
               className={`flex-1 mx-1 py-2 rounded-full items-center justify-center transition-all duration-150 ${
-                isActive ? "bg-black" : " bg-transparent"
+                isActive ? "bg-black" : "bg-transparent"
               }`}
             >
-              <Text
-                className={`font-semibold text-sm ${
-                  isActive ? "text-white" : "text-gray-700"
-                }`}
-              >
-                {tab.name}
-              </Text>
+              <Icon
+                size={24}
+                color={isActive ? "white" : "gray"}
+                strokeWidth={2.4}
+              />
             </TouchableOpacity>
           </Link>
         );
